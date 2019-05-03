a7f88cada6aaa67f9bcd716da475f1248a0a1d6a
yaskev <yaskevich.san@mail.ru>
2019-05-03 15:41:16 +0300

    DDL instructions

---
+++ DB_init.sql
@@ -0,0 +1,82 @@
+set search_path to trading;
+
+create table Company (
+    company_id integer primary key,
+    company_nm varchar(50) not null,
+    address_txt varchar(50)
+);
+
+create table Broker (
+    broker_id integer primary key,
+    broker_nm varchar(50) not null,
+    address_txt varchar(50),
+    commission_amt real check ( commission_amt >= 0 )
+);
+
+create table Trader (
+    trader_id integer primary key,
+    trader_nm varchar(25) not null,
+    address_txt varchar(50),
+    joined_dt timestamp not null,
+    birth_dt timestamp not null,
+    is_vip_flg boolean
+);
+
+create table Account (
+    account_id integer primary key,
+    balance_amt real default 0.0,
+    trader_id integer references Trader(trader_id),
+    account_no integer not null unique,
+    currency_nm varchar(3) not null,
+    valid_from_dttm timestamp not null,
+    valid_to_dttm timestamp not null
+);
+
+create table Stock (
+    ticker_id varchar(4) primary key,
+    company_id integer not null references Company(company_id)
+);
+
+create table Exchange (
+    exchange_id integer primary key,
+    exchange_nm varchar(50) not null,
+    address_txt varchar(50),
+    commission_amt real check (commission_amt >= 0)
+);
+
+create table Stock_X_Account (
+    ticker_id varchar(4) references Stock(ticker_id),
+    exchange_id integer references Exchange(exchange_id),
+    account_id integer references Account(account_id),
+    stock_cnt integer check ( stock_cnt > 0 ),
+    transaction_dttm timestamp,
+    transaction_type varchar(4) check ( transaction_type in ('BUY', 'SELL') ),
+    primary key (ticker_id, exchange_id, account_id)
+);
+
+create table Stock_X_Exchange (
+    ticker_id varchar(4) references Stock(ticker_id),
+    exchange_id integer references Exchange(exchange_id),
+    price_amt real check ( price_amt > 0 ),
+    valid_from_dt timestamp,
+    valid_to_dt timestamp,
+    primary key (ticker_id, exchange_id)
+);
+
+create table Account_X_Exchange (
+    exchange_id integer references Exchange(exchange_id),
+    account_id integer references Account(account_id),
+    primary key (exchange_id, account_id)
+);
+
+create table Exchange_X_Broker (
+    exchange_id integer references Exchange(exchange_id),
+    broker_id integer references Broker(broker_id),
+    primary key (exchange_id, broker_id)
+);
+
+create table Trader_X_Broker (
+    trader_id integer references Trader(trader_id),
+    broker_id integer references Broker(broker_id),
+    primary key (trader_id, broker_id)
+);
