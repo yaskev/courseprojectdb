1de1575a7e902c8791f9298c89542070eecef345
yaskev <yaskevich.san@mail.ru>
2019-05-04 13:00:46 +0300

    CRUD

--- CRUD.sql
+++ CRUD.sql
@@ -1,2 +1,13 @@
 set search_path to trading;
 
+-- Table account
+insert into account values (8, 5, 1008, 'USD', timestamp'05-04-2019');
+select * from account;
+update account set currency_nm = 'EUR' where account_no = 1008;
+delete from account where account_no = 1008;
+
+-- Table trader
+insert into trader values (7, 'Mickael Lewees', 'France', timestamp'05-04-2019', timestamp'11-05-1987');
+update trader set broker_id = 5 where trader_id = 7;
+select * from trader;
+delete from trader where trader_id = 7;
